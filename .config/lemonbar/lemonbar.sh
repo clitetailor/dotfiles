340feec735d3f0bc4c2fd530b1bd1201c842506c
clitetailor <ducnhatphung@gmail.com>
2019-08-11 19:57:10 +0700

    fix: missing

--- .config/lemonbar/lemonbar.sh
+++ .config/lemonbar/lemonbar.sh
@@ -1,1 +1,2 @@
-admiral -c ../admiral.d/admiral.toml | lemonbar -f "Dejavu Sans Mono:size=11" -f "ShureTechMono Nerd Font:size=13" -B "#40000000" -g x28++ -p | sh 
+admiral -c $XDG_CONFIG_HOME/admiral.d/admiral.toml | lemonbar -o -1 -f "Dejavu Sans Mono:size=11" -o 0 -f "ShureTechMono Nerd Font:size=13" -B "#40000000" -g x28++ -p | sh
+
